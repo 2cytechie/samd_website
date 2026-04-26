c6b35f8e86a7c06d5bc3b5a7de352c55eaf4dcfe
sam <bindwood27@hotmail.com>
2026-04-26 14:14:26 +0800

    添加赛车游戏

--- pages/Works/Works.tsx
+++ pages/Works/Works.tsx
@@ -1,4 +1,5 @@
 import './Works.css';
+import { worksData } from '../data/worksData';
 
 function Works() {
   return (
@@ -6,6 +7,15 @@ function Works() {
       <div className="works-content">
         <h1 className="works-title">我的作品</h1>
         <p className="works-subtitle">精彩即将呈现...</p>
+        <div className="works-grid">
+          {worksData.map((work) => (
+            <div key={work.id} className="work-card">
+              <a href={work.workUrl} target="_blank" rel="noopener noreferrer">
+                <img src={work.thumbnail} alt={work.title} className="work-thumbnail" />
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
